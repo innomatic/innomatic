35b8830918b6233aa989c02c3a4f34f1cca93ab5
Alex Pagnoni <alex.pagnoni@innoteam.it>
2015-03-25 12:04:29 +0100

    Added Innomatic Legacy Bundle

---
+++ src/Bundle/InnomaticLegacyBundle/InnomaticLegacyBundle.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Innomatic
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.
+ *
+ * @copyright  2014-2015 Innomatic Company
+ * @license    http://www.innomatic.io/license/ New BSD License
+ * @link       http://www.innomatic.io
+ */
+
+namespace Innomatic\Bundle\InnomaticLegacyBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Innomatic\Bundle\InnomaticLegacyBundle\DependencyInjection\InnomaticLegacyExtension;
+
+class InnomaticLegacyBundle extends Bundle
+{
+    public function getContainerExtension()
+    {
+        if ( !isset( $this->extension ) )
+        {
+            $this->extension = new InnomaticLegacyExtension(
+                array()
+            );
+        }
+
+        return $this->extension;
+    }
+}
